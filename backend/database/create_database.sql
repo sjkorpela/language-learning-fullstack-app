288c0fe3b51b1794a46ca60e3589edddb6e5b1f3
Santeri Korpela <143890623+sjkorpela@users.noreply.github.com>
2025-01-14 12:52:48 +0200

    Update database creation SQL script
    Edited to better reflect the current project scope

--- backend/database/create_database.sql
+++ backend/database/create_database.sql
@@ -5,40 +5,56 @@ CREATE DATABASE llfsa;
 
 USE llfsa;
 
--- Languages could be stored as an SQL enum?
-
--- Create the tests table:
--- - id           -> Id of the test, as an integer 0 or greater
--- - test_lang    -> Language that the test is testing, as a 3 char string using ISO 639-3 language codes, https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes
--- - answ_lang    -> Language that the test is answered in, as the same as test_lang
--- - title        -> Name of the test, as a string 5-50 chars long
--- - descript     -> Description of the test's subject, as a text
--- - last_edited  -> When the test was last edited, as a timestamp
--- - edited_by(?) -> Who the test was edited by, as a user id
-CREATE TABLE tests(
+-- Create the words table:
+-- - id       -> Id of the word, as an integer 1 or greater
+-- - fooWord  -> The left side word, string max 30 characters
+-- - fooLang  -> Language that the word, as a 3 char string using ISO 639-3 language codes set 3, https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes
+-- - barWord  -> The right side word, string max 30 characters
+-- - barLang  -> Language that the word, as a 3 char string same as fooLang
+-- - tags     -> Tags of the ford, as a string with format #,#,# where each # is a tag id, see tags table
+CREATE TABLE words(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  test_lang CHAR(3) NOT NULL,
-  answ_lang CHAR(3) NOT NULL,
-  title VARCHAR(50) NOT NULL,
-  descript TEXT NOT NULL,
-  last_edited TIMESTAMP NOT NULL
-  -- edited_by INT NOT NULL,
-  -- FOREIGN KEY(edited_by) REFERENCES user(id)
+  fooWord VARCHAR(30) NOT NULL,
+  fooLang CHAR(3) NOT NULL,
+  barWord VARCHAR(30) NOT NULL,
+  barLang CHAR(3) NOT NULL,
+  tags  VARCHAR(30) NOT NULL
 );
 
--- Test taker is just a string -> taking the test for someone else is possible, maybe fix later?
-
--- Create the submissions table:
--- - id          -> Id of the submission, as an integer 0 or greater
--- - test_id     -> Which test this is a submission for, as a test id
--- - taker       -> Who the submissions is by, as a string 5-50 chars long
--- - submit_time -> Time the submission is sent, as a timestamp
-CREATE TABLE submissions(
+-- Create the tags table:
+-- - id       -> Id of the tag, as an integer 1 or greater
+-- - fooWord  -> Name of the tag, string max 30 characters
+CREATE TABLE tags(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  test_id INT NOT NULL, FOREIGN KEY(test_id) REFERENCES tests(id),
-  taker VARCHAR(50) NOT NULL,
-  submit_time TIMESTAMP NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
-INSERT INTO tests(id, test_lang, answ_lang, title, descript, last_edited) VALUES
-  (0, "eng", "fin", "Test test", "A test test to test the test table.", "2024-12-13 14:10:00")
+-- Insert example data
+
+INSERT INTO tags(name) VALUES
+  ("Animals"),
+  ("Fruits"),
+  ("Computer science")
+
+-- Languages
+-- eng -> english
+-- fin -> finnish
+-- swe -> swedish
+-- uni -> unicode character
+
+-- Tags
+-- 1 -> animals
+-- 2 -> fruits
+-- 3 -> computer science
+
+INSERT INTO words(fooWord, fooLang, barWord, barLang, tags) VALUES
+  ("cat", "eng", "kissa", "fin", "1"),
+  ("hund", "swe", "koira", "dog", "1"),
+  ("kivi", "swe", "kiivi", "fin", "1,2"),
+  ("pineapple", "eng", "ananas", "fin", "2"),
+  ("unemployed", "eng", "työtön", "fin", "3"),
+  ("olio", "fin", "object", "eng", "3"),
+  ("()", "uni", "kaarisulkeet", "fin", "3"),
+  ("[]", "uni", "hakasulkeet", "fin", "3"),
+  ("{}", "uni", "aaltosulkeet", "fin", "3"),
+  ("〈〉", "uni", "kulmasulkeet", "fin", "3")
