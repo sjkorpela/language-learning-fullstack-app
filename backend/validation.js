00ae33cb91dba90a542e0dd82c2b89563c4a62d2
Santeri Korpela <143890623+sjkorpela@users.noreply.github.com>
2025-01-14 13:54:08 +0200

    Update validation.js
    Added validation for words, tags, language codes, and table names.
    
    Also changed minimum id from 0 to 1 because SQL auto increment starts at 1 so 0 isn't actually a valid id.

--- backend/validation.js
+++ backend/validation.js
@@ -1,7 +1,17 @@
 const Joi = require("joi");
 
+// IDs must be integers 1 or greater
+// Languages must be exactly 3 characters
+// Words, tag names, and taglists must be between 1 and 30 characters, nts: change to 50?
+// Table name must be one of existing ones
+
 const schema = Joi.object({
-  id: Joi.number().integer().min(0),
+  id: Joi.number().integer().min(1),
+  lang: Joi.string().min(3).max(3),
+  word: Joi.string().min(1).max(30),
+  tag: Joi.string().min(1).max(30),
+  tags: Joi.string().min(1).max(30),
+  table: Joi.string().valid("words", "tags")
 });
 
 module.exports = schema;
